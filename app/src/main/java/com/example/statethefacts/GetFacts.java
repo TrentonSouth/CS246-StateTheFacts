c71353771906c44e9ed2d9e89eda6f136a720d94
trentonsouth <trenton.south@gmail.com>
2020-10-20 15:55:46 -0500

    GetFacts is working

--- app/src/main/java/com/example/statethefacts/GetFacts.java
+++ app/src/main/java/com/example/statethefacts/GetFacts.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.util.Log;
 import com.google.firebase.database.*;
 import com.google.gson.Gson;
+import org.json.JSONObject;
 
 
 import java.lang.ref.WeakReference;
+import java.util.HashMap;
 
 public class GetFacts extends Thread {
     private WeakReference<Activity> activityRef;
@@ -18,17 +20,20 @@ public class GetFacts extends Thread {
     @Override
     public void run() {
         FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
-        DatabaseReference mDbRef = mDatabase.getReference("/");
+        DatabaseReference mDbRef = mDatabase.getReference("/facts");
         mDbRef.addValueEventListener(new ValueEventListener() {
 
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                String json = dataSnapshot.getValue().toString();
-                //Gson gson = new Gson();
-                //Facts facts = gson.fromJson(json, Facts.class);
-                //Log.d("TAG", "Value is: " + facts.states.get(0).abbreviation);
-                Log.d("TAG", "Value is: " + json);
+
+                Gson gson = new Gson();
+                HashMap<String, JSONObject> dataSnapshotValue = (HashMap<String, JSONObject>) dataSnapshot.getValue();
+                String json = new Gson().toJson(dataSnapshotValue);
+                Facts facts = gson.fromJson(json, Facts.class);
+                Log.d("Message:", "Value is: " + json);
+                Log.d("Message:", "Value is: " + facts.states.get(0).abbreviation);
+
             }
 
             public void onCancelled(DatabaseError error) {
