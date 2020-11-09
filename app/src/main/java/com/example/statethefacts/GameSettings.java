7a85ef4850e7acbeec19abefde27878629c7f0d9
Christine Noonan <jo.christine@gmail.com>
2020-11-09 18:54:35 -0500

    Christine worked on PracticeModeReviewPresenter.java

--- app/src/main/java/com/example/statethefacts/GameSettings.java
+++ app/src/main/java/com/example/statethefacts/GameSettings.java
@@ -3,89 +3,15 @@ package com.example.statethefacts;
 import java.util.List;
 
 public class GameSettings {
-    Boolean Name;  //this is the State
-    Boolean Capital;
-    Boolean Flower;
-    Boolean Rock;
-    Boolean Bird;
-    Boolean Governor;
     Boolean multipleChoice;
     List<State> states;
 
-    public GameSettings(Boolean name, Boolean capital, Boolean flower, Boolean rock, Boolean bird, Boolean governor, Boolean multipleChoice, List<State> states) {
-        Name = name;
-        Capital = capital;
-        Flower = flower;
-        Rock = rock;
-        Bird = bird;
-        Governor = governor;
+    public GameSettings(Boolean multipleChoice, List<State> states) {
         this.multipleChoice = multipleChoice;
         this.states = states;
     }
 
-    public Boolean getName() {
-        return Name;
-    }
 
-    public void setName(Boolean name) {
-        Name = name;
-    }
-
-    public Boolean getCapital() {
-        return Capital;
-    }
-
-    public void setCapital(Boolean capital) {
-        Capital = capital;
-    }
-
-    public Boolean getFlower() {
-        return Flower;
-    }
-
-    public void setFlower(Boolean flower) {
-        Flower = flower;
-    }
-
-    public Boolean getRock() {
-        return Rock;
-    }
-
-    public void setRock(Boolean rock) {
-        Rock = rock;
-    }
-
-    public Boolean getBird() {
-        return Bird;
-    }
-
-    public void setBird(Boolean bird) {
-        Bird = bird;
-    }
-
-    public Boolean getGovernor() {
-        return Governor;
-    }
-
-    public void setGovernor(Boolean governor) {
-        Governor = governor;
-    }
-
-    public Boolean getMultipleChoice() {
-        return multipleChoice;
-    }
-
-    public void setMultipleChoice(Boolean multipleChoice) {
-        this.multipleChoice = multipleChoice;
-    }
-
-    public List<State> getStates() {
-        return states;
-    }
-
-    public void setStates(List<State> states) {
-        this.states = states;
-    }
 
 
 }
--- app/src/main/java/com/example/statethefacts/GameSettingsPresenter.java
+++ app/src/main/java/com/example/statethefacts/GameSettingsPresenter.java
@@ -1,19 +1,5 @@
 package com.example.statethefacts;
 
-import android.os.Build;
-
-import androidx.annotation.RequiresApi;
-
-import com.google.gson.Gson;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 
 public class GameSettingsPresenter {
     GameSettings gameSettings;
@@ -33,40 +19,6 @@ public class GameSettingsPresenter {
     public void startGame() {
 
     }
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    public void run() {
-        Gson gson = new Gson();
-        HttpURLConnection httpURLConnection;
-        final String stateTheFactsURL = "http://www.statethefacts.us/admin/";
-
-        try {
-            URL url = new URL(stateTheFactsURL);
-            httpURLConnection = (HttpURLConnection) url.openConnection();
-            httpURLConnection.setRequestMethod("GET");
-            httpURLConnection.setConnectTimeout(10000);
-            httpURLConnection.setReadTimeout(10000);
-            int status = httpURLConnection.getResponseCode();
-            System.out.println("HTTP Response code: " + status);
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            InputStream inputStream = new URL(stateTheFactsURL).openStream();
-            Reader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-            gameSettings = gson.fromJson(reader, GameSettings.class);
-        } catch (Exception exception) {
-        }
-
-    }
-
-    /*public void GameSettingsActivity(View view) {
-
-    }*/
 
 
 }
