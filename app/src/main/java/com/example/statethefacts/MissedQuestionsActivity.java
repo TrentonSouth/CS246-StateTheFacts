31f737ca972333a382a4bd75b8cc3ef7540aa7e5
trentonsouth <trenton.south@gmail.com>
2020-12-07 16:49:17 -0600

    Using MissedQuestionsPresenter to build display.

--- app/src/main/java/com/example/statethefacts/MissedQuestionsActivity.java
+++ app/src/main/java/com/example/statethefacts/MissedQuestionsActivity.java
@@ -10,24 +10,15 @@ import android.widget.TextView;
 import java.util.List;
 
 public class MissedQuestionsActivity extends AppCompatActivity {
-
+    private MissedQuestionsPresenter presenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_missed_questions);
+        presenter = new MissedQuestionsPresenter(this);
 
-        GameResult gr = new GameResult().loadCurrentGame(this);
-        List<GameAnswer> gameAnswers = gr.getAnswers();
-        String display = "";
-        for(GameAnswer ga : gameAnswers) {
-            if(!ga.HasCorrectAnswer()) {
-                display += ga.getState() + " - " + ga.getQuestionType() + "\n";
-                display += "Your Answer: " + ga.getSubmittedAnswer() + "\n";
-                display += "Correct Answer: " + ga.getCorrectAnswer() + "\n\n";
-            }
-        }
         TextView txtMissedQuestions = this.findViewById(R.id.txtMissedQuestions);
-        txtMissedQuestions.setText(display);
+        txtMissedQuestions.setText(presenter.getDisplay(this));
     }
 
     public void mainMenu(View view) {
