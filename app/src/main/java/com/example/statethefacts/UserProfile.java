66c55481c8bad793e3d748ce294a30a684760cc2
mikegibson66 <mikegibson66@byui.edu>
2020-12-09 15:42:02 -0700

    update UserProfile to include the User's Age

--- app/src/main/java/com/example/statethefacts/UserProfile.java
+++ app/src/main/java/com/example/statethefacts/UserProfile.java
@@ -1,15 +1,29 @@
 
 package com.example.statethefacts;
 
+/**
+ * UserProfile is a class to hold user information, including
+ * name, email and age. It is a basic class with just the three
+ * fields, getters, setters, and two constructors.
+ */
 public class UserProfile {
     private String userName;
     private String userEMail;
+    private int userAge;
 
+    // constructors
     public UserProfile(String userName, String userEMail) {
         setUserName(userName);
         setUserEMail(userEMail);
+        setUserAge(0);
+    }
+    public UserProfile(String userName, String userEMail, int userAge) {
+        setUserName(userName);
+        setUserEMail(userEMail);
+        setUserAge(userAge);
     }
 
+    // getters and setters
     public String getUserName() {
         return userName;
     }
@@ -23,4 +37,7 @@ public class UserProfile {
     public void setUserEMail(String userEMail) {
         this.userEMail = userEMail;
     }
+
+    public int getUserAge() { return userAge; }
+    public void setUserAge(int userAge) { this.userAge = userAge; }
 }
