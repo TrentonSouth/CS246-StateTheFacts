6e0d787a6229c28368b10bbc71102a331934d9c0
mikegibson66 <mikegibson66@byui.edu>
2020-12-08 19:12:28 -0700

    Update MainUserProfilePresenter with documentation

--- app/src/main/java/com/example/statethefacts/MainUserProfilePresenter.java
+++ app/src/main/java/com/example/statethefacts/MainUserProfilePresenter.java
@@ -1,10 +1,14 @@
 package com.example.statethefacts;
 
-import android.app.Activity;
 import android.content.SharedPreferences;
 import android.util.Log;
 import android.widget.TextView;
 
+/**
+ * MainUserProfilePresenter cast is the user profile presenter for
+ * MainActivity. Provides MainActivity with methods and access to fields for
+ * the User's name and Email address.
+ */
 public class MainUserProfilePresenter {
     private static final String TAG = "MainUserProfile";
     private MainActivity activity;
@@ -14,6 +18,11 @@ public class MainUserProfilePresenter {
         return profile;
     }
 
+    /**
+     * MainUserProfilePresenter constructor creates an object that contains user profile
+     * information, if it exists and updates the welcome screen with the user's name
+     * @param activity
+     */
     public MainUserProfilePresenter(MainActivity activity) {
         this.activity = activity;
         SharedPreferences preferences = activity.getSharedPreferences("STFUserProfile", 0);
@@ -21,6 +30,8 @@ public class MainUserProfilePresenter {
                 preferences.getString("user_email", null));
         Log.i(TAG, "Loaded user information from STFUserProfile");
 
+        // if the user has input their name as part of the profile, the welcome screen
+        // gets updated with their name.
         if (profile.getUserName() != null) {
             TextView editUserWelcome = activity.findViewById(R.id.user_welcome);
             String welcomeText = "Welcome back, " + profile.getUserName() + "!";
