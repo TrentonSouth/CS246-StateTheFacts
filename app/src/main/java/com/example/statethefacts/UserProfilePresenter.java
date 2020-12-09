92f75a5bb0c420e4d8f5b84fdb1d95013dcbe581
mikegibson66 <mikegibson66@byui.edu>
2020-12-08 19:22:39 -0700

    Update MainUserProfilePresenter with documentation

--- app/src/main/java/com/example/statethefacts/UserProfilePresenter.java
+++ app/src/main/java/com/example/statethefacts/UserProfilePresenter.java
@@ -1,45 +1,20 @@
 
 package com.example.statethefacts;
 
-import android.content.SharedPreferences;
-import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UserProfilePresenter extends AppCompatActivity {
     private UserProfile profile;
 
-    public UserProfile getProfile() {
-        return profile;
-    }
-    public void setProfile(UserProfile profile) {
-        this.profile = profile;
-    }
     public String getUserName() {
         return profile.getUserName();
     }
+
     public String getUserEMail() {
         return profile.getUserEMail();
-    }
-    public void setUserName(String userName) {
-        profile.setUserName(userName);
-    }
-    public void setUserEMail(String userEMail) {
-        profile.setUserEMail(userEMail);
     }
 
     public UserProfilePresenter(String user, String email) {
         profile = new UserProfile(user, email);
-    }
-    // save profile information to shared preferences
-    public void saveProfile(View view) {
-
-    }
-    // get profile information from shared preferences
-    public void loadProfile() {
-        // load preference file
-        SharedPreferences preferences = getApplicationContext().getSharedPreferences("userProfile",0);
-        UserProfile newProfile = new UserProfile(preferences.getString("user_name", null),preferences.getString("user_email", null));
-        setProfile(newProfile);
     }
 }
