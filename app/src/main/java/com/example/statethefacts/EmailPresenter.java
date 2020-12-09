6ee584f91638fd482bb552f8c3ad3b740cf724c7
mikegibson66 <mikegibson66@byui.edu>
2020-12-08 18:26:42 -0700

    Document EmailPresenter

--- app/src/main/java/com/example/statethefacts/EmailPresenter.java
+++ app/src/main/java/com/example/statethefacts/EmailPresenter.java
@@ -3,6 +3,10 @@ package com.example.statethefacts;
 import android.content.SharedPreferences;
 import android.util.Log;
 
+/**
+ * The EmailPresenter class contains the methods used to send emails, linking
+ * GMail and SendMailTask classes to the HistoryActivity
+ */
 public class EmailPresenter {
     private static final String FROM_EMAIL = "statethefacts2020@gmail.com";
     private static final String EMAIL_PASSWORD = "H9F4eBNA2wStNs2F";
@@ -15,6 +19,11 @@ public class EmailPresenter {
     private String emailBody;
     private String emailSubject;
 
+    /**
+     * EmailPresenter() constructor makes HistoryActivity available to an EmailPresenter object.
+     * Then, lodes user information from sharedpreferences into a UserProfilePresenter object.
+     * @param activity
+     */
     public EmailPresenter(HistoryActivity activity) {
         this.activity = activity;
         SharedPreferences preferences = activity.getSharedPreferences("STFUserProfile", 0);
@@ -23,10 +32,19 @@ public class EmailPresenter {
 
     }
 
+    /**
+     * getProfile() is a getter fro the UserProfilePresenter object profile
+     * @return
+     */
     public UserProfilePresenter getProfile() {
         return profile;
     }
 
+    /**
+     * sendMessage() method checks content of the UserProfilePresenter object.
+     * If it is not null, has user information, it will send the game summary in
+     * an email to the user's email address.
+     */
     public void sendMessage() {
         if (profile.getUserName() != null && profile.getUserEMail() != null) {
             Log.i(TAG, "Send History Email Launched");
