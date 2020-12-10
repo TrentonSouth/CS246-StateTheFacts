6ad5608fb24faf122f0da97b8e24be10701f9667
mikegibson66 <mikegibson66@byui.edu>
2020-12-10 08:47:43 -0700

    Activate ellipsis in MissedQuestionsActivity

--- app/src/main/java/com/example/statethefacts/MissedQuestionsActivity.java
+++ app/src/main/java/com/example/statethefacts/MissedQuestionsActivity.java
@@ -1,5 +1,6 @@
 package com.example.statethefacts;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,6 +8,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -22,7 +24,9 @@ import java.util.List;
  * @since 12/8/2020
  */
 public class MissedQuestionsActivity extends AppCompatActivity {
+    public static final String GAME_MODE = "com.example.statethefacts.GAME_MODE";
     private MissedQuestionsPresenter presenter;
+    Intent intent;
 
     // When the class is instantiated, the presenter is created
     // and the display is built from getDisplay()
@@ -48,7 +52,48 @@ public class MissedQuestionsActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //https://www.youtube.com/watch?v=kknBxoCOYXI
-        getMenuInflater().inflate(R.menu.main_menu, menu);
+        getMenuInflater().inflate(R.menu.missedquestion_menu, menu);
+        return true;
+    }
+    /**
+     * onOptionsItemSelected Method
+     * Purpose: to listen to the user action when they select a menu item from the 3 dots
+     * (ellipsis) on the upper right side of the app, in the top bar. Navigates to specific
+     * activity based on user selection
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.main:
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.profile:
+                intent = new Intent(this, UserProfileActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.history:
+                intent = new Intent(this, HistoryActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.score_card:
+                intent = new Intent(this, ScoreCardActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.learn_mode:
+                intent = new Intent(this, FactsListActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.game_mode:
+                intent = new Intent(this, GameSettingsActivity.class);
+                intent.putExtra(GAME_MODE, "game");
+                startActivity(intent);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
         return true;
     }
 
